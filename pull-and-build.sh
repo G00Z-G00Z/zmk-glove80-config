86c01f4ad1cdbf98670a7ac775faa36af821d221
G00Z_G00Z <edygomezgg4@gmail.com>
2025-04-04 09:57:22 -0600

    Pull and build script

---
+++ pull-and-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git pull
+set -euo pipefail
+
+python ./scripts/update-max-combo.py ./config/glove80.keymap ./config/glove80.conf
+
+IMAGE=glove80-zmk-config-docker
+BRANCH="${1:-main}"
+
+docker build -t "$IMAGE" .
+docker run --rm -v "$PWD:/config" -e UID="$(id -u)" -e GID="$(id -g)" -e BRANCH="$BRANCH" "$IMAGE"
+open .
